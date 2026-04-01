e77c42f266901007def3f535b420c9b2bdb7f847
Aman Yadav <aman.yadav.st.johns@gmail.com>
2026-04-01 20:02:52 +0530

    Implemented Attempt Entity

---
+++ src/main/java/com/quiz/Quiz/Application/Entity/Attempt.java
@@ -0,0 +1,30 @@
+package com.quiz.Quiz.Application.Entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Attempt {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Integer score;       // Kitne sawal sahi hue
+    private Integer totalQuestions; // Total kitne sawal the
+    private LocalDateTime timestamp; // Kab attempt kiya (Date & Time)
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;          // Kis user ne attempt kiya
+
+    @ManyToOne
+    @JoinColumn(name = "quiz_id")
+    private Quiz quiz;          // Kaunsa quiz attempt kiya
+}
