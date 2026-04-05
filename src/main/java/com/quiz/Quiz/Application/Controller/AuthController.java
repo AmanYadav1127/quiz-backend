585fa5466f4ed138218dcdcef45bdf6537b9ed93
Aman Yadav <aman.yadav.st.johns@gmail.com>
2026-04-06 00:07:56 +0530

    Little changes

--- src/main/java/com/quiz/Quiz/Application/Controller/AuthController.java
+++ src/main/java/com/quiz/Quiz/Application/Controller/AuthController.java
@@ -13,10 +13,7 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -24,6 +21,7 @@ import java.util.Map;
 @RestController
 @RequestMapping("/api/auth")
 @RequiredArgsConstructor
+@CrossOrigin("*")
 public class AuthController {
     private final AuthenticationManager authenticationManager;
     private final UserRepository userRepository;
--- src/main/java/com/quiz/Quiz/Application/Controller/MLController.java
+++ src/main/java/com/quiz/Quiz/Application/Controller/MLController.java
@@ -14,6 +14,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/ml")
 @RequiredArgsConstructor
+@CrossOrigin("*")
 public class MLController {
 
     private final MLService mlService;
